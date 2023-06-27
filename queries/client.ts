ad017b2032edf654bc4c6a4edc17c6022c2d34c6
Shibbaz <kmosc@protonmail.com>
2023-06-28 01:00:58 +0200

    sample subscription

--- queries/client.ts
+++ queries/client.ts
@@ -1,32 +1,43 @@
-import { createTRPCProxyClient, httpBatchLink, createWSClient, wsLink } from '@trpc/client';
+import { createTRPCProxyClient, httpBatchLink, createWSClient, wsLink, loggerLink, splitLink } from '@trpc/client';
 import type { AppRouter } from './index';
 import ws from 'ws';
-
 globalThis.WebSocket = ws as any;
 const wsClient = createWSClient({
-  url: `ws://localhost:4000`,
+  url: `ws://localhost:3000`,
 });
 const trpc = createTRPCProxyClient<AppRouter>({
   links: [
-    wsLink<AppRouter>({ client: wsClient })],
+    loggerLink(),
+    // call subscriptions through websockets and the rest over http
+    splitLink({
+      condition(op) {
+        return op.type === 'subscription';
+      },
+      true: wsLink({
+        client: wsClient,
+      }),
+      false: httpBatchLink({
+        url: `http://localhost:3000`,
+      }),
+    }),
+  ],
 });
 var count = 0;
 async function main(){
-    const userList = await trpc.usersList.query()
-    console.log("userList:", userList)
-    const helloUser = await trpc.helloUser.query({ text: 'kamil' })
-    console.log("Hello message: ", helloUser)
     const changeUser = await trpc.changeFirstUser.mutate({text: "Shibbaz"})
-    const updatedUserList = await trpc.usersList.query()
-    console.log("userList:", updatedUserList)
-    const trpcsub = await new Promise<void>((resolve) => {
-      const subscription = trpc.onUpadateUser.subscribe(undefined, {
-        onData(data) {
-          console.log(data);
-      }})
-      
-    });
-    wsClient.close();  
+    await new Promise<void>((resolve) => {
+      const subscription = trpc.onUpadateUser.subscribe({id: 0}, {
+        onData(data): void {
+          console.log("Updated ", data)
+        },
+        onError(err) {
+          console.error('Subscription error:', err);
+        },
+      }
+      )
+    })
+    wsClient.close();
+
 }
 
 main().catch(console.error)
--- queries/index.ts
+++ queries/index.ts
@@ -1,58 +1,23 @@
 import { createHTTPServer } from '@trpc/server/adapters/standalone';
-import * as trpc from '@trpc/server';
-import { publicProcedure, router } from './trpc';
-import {z} from 'zod'
-import { EventEmitter } from 'events';
-import { observable } from '@trpc/server/observable';
-
-const users = [
-    {
-        name: "Kamil",
-        age: 25
-    }
-]
-const ee = new EventEmitter();
-
-export const appRouter = router({
-    helloUser: publicProcedure.input(
-        z.object({
-          text: z.string().nullish(),
-        })).query(({ input }) => {
-            return {
-                text: `Hello ${ input.text }`
-            }
-        }),
-    usersList: publicProcedure.query(async() => {
-        return users
-    }),
-    changeFirstUser: publicProcedure.input(
-        z.object({
-            text: z.string().nullish(),
-        })
-    ).mutation(({ input }:any) => {
-        users[0].name=input.text
-        ee.emit('change', users[0]);
-
-    }),
-    onUpadateUser: publicProcedure.subscription(()=> {
-        return observable<JSON>((emit) => {
-            const onChange = (data: JSON) => {
-                emit.next(data);
-            }
-            ee.on('change', onChange);
-            return () => {
-                ee.off('change', onChange);
-              };
-        })
-    })
-
-
-    }
-);
-
+import { appRouter } from './router'
 export type AppRouter = typeof appRouter;
-const server = createHTTPServer({
+import { applyWSSHandler } from '@trpc/server/adapters/ws';
+import ws from 'ws';
+const { server, listen } = createHTTPServer({
     router: appRouter,
   });
-   
 console.log("Server on")
+
+const wss = new ws.Server({
+  server
+});
+const handler = applyWSSHandler({ wss, router: appRouter});
+
+wss.on('connection', (ws) => {
+  console.log(`➕➕ Connection (${wss.clients.size})`);
+  ws.once('close', () => {
+    console.log(`➖➖ Connection (${wss.clients.size})`);
+  });
+});
+console.log('✅ WebSocket Server listening on ws://localhost:3000');
+listen(3000);
